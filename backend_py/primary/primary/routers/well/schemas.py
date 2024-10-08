68b200b15feaf789f68e8ece343bf52938ac80a2
Anders Rantala Hunderi <anders@ceetronsolutions.com>
2024-10-08 15:39:02 +0200

    Added log name field to log curve header endpoint

--- backend_py/primary/primary/routers/well/schemas.py
+++ backend_py/primary/primary/routers/well/schemas.py
@@ -108,6 +108,7 @@ class WellboreLogCurveHeader(BaseModel):
 
 class WellboreLogCurveData(BaseModel):
     name: str
+    logName: str
     indexMin: float
     indexMax: float
     minCurveValue: float
